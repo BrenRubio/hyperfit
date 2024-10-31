0687fa43af4784bdc63e4f74c09706c0d7d29e68
Brenda Rubio <brenrglez@gmail.com>
2024-10-31 02:13:58 -0600

    Almacenamiento de actividades acompletadas de resistencia en BD

--- lib/paginas/plan_peso.dart
+++ lib/paginas/plan_peso.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Plan - Resistencia',
+      title: 'Plan - Perder peso',
       home: PlanPeso(),
     );
   }
